81626f163a1d1966b816b0f4d73f728506b9752d
DingJian <DingJian@DESKTOP-V0619C5>
2018-08-31 17:41:52 +0800

    JewelsandStones

---
+++ Jewels and Stones/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jewels_and_Stones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string J = "z";
+            string S = "ZZ";
+
+            Console.WriteLine(NumJewelsInStones(J, S));
+            Console.ReadKey();
+            
+
+        }
+
+        public static int NumJewelsInStones(string J, string S)
+        {
+            int num = 0;
+            foreach (var charc in J)
+            {
+                num += S.Where(str => str.Equals(charc)).Count();
+            }
+
+            return num;
+        }
+    }
+}
